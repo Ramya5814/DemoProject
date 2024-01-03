47a31b3e7b04334b41fbb16c66cd34dad44f2de5
Ramya5814 <ramyabalegara@gmail.com>
2024-01-03 12:04:38 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,12 +1,5 @@
-def generateEmailContent(build) {
-    """
-    <html>
-        <body>
-            <h2>Build Status: ${build.result}</h2>
-            <p>Build Number: ${build.number}</p>
-            <p>Build URL: <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>
-            <p>Custom Message: Your custom message goes here.</p>
-        </body>
-    </html>
-    """
+def printMessage() {
+    echo "Hello from myScript.groovy!"
 }
+
+return this
